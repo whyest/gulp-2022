52af56e8f9c22a07f049f2e7ec4397d2ebacdc74
Денис Галкин <whyest@gmail.com>
2022-09-02 15:28:07 +0300

    Added git file include

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,5 +1,8 @@
+import fileInclude from 'gulp-file-include';
+
 export const html = () => {
 	return app.gulp
 		.src(app.path.src.html)
-		.pipe(app.gulp.dest(app.path.build.html));
+		.pipe(fileInclude())
+		.pipe(app.gulp.dest('dist'));
 };
